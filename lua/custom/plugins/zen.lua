a3a06948176b2f3837b960e26fdef3fec61ca901
patrick.eschenbach <patrick.eschenbach@covestro.com>
2025-12-16 15:46:36 +0100

    added toggle for zen mode
    Signed-off-by: patrick.eschenbach <patrick.eschenbach@covestro.com>

--- lua/custom/plugins/zen.lua
+++ lua/custom/plugins/zen.lua
@@ -1,8 +1,9 @@
 return {
   'folke/zen-mode.nvim',
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+  opts = {},
+  config = function()
+    vim.keymap.set('n', '<C-W>z', function()
+      require('zen-mode').toggle()
+    end, { desc = 'Toggle Zen Mode' })
+  end,
 }
